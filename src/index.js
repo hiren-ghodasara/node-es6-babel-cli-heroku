08410fffc125d86e7be587a24f3c551835f3af09
Jalpesh <jalpesh@albiorixtech.com>
2020-07-02 11:45:25 +0530

    make it better

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ app.get('/', (req, res) => {
     res.status(200).json({ message: `Welcome to Node.js & Express 11` });
 });
 app.listen(process.env.PORT || 3000, () => {
-    console.log("Listening to port 3000")
+    console.log("Listening to port 3000",process.env.NPM_CONFIG_PRODUCTION)
 });
