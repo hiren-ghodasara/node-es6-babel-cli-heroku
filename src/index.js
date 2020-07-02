f22fe73aa3d86029dc25d3d0bc9141f6c0399d93
Jalpesh <jalpesh@albiorixtech.com>
2020-07-02 10:57:58 +0530

    Code deploy on heroku

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import express from 'express';
+const app = express();
+app.get('/', (req, res) => {
+    res.status(200).json({ message: `Welcome to Node.js & Express` });
+});
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Listening to port 3000")
+});
